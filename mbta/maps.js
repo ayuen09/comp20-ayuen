7e964532cb1cea2e6deab56fc626f2a1de1d4500
Anna <wingsyuenanna@gmail.com>
2018-10-23 17:51:43 -0400

    adjusted infowindow size

--- mbta/maps.js
+++ mbta/maps.js
@@ -1,25 +1,25 @@
 var locationList = [ ['South Station', 42.352271, -71.05524200000001, 'place-sstat'],
-				['Andrew'           ,   42.330154 ,         -71.057655 , 'place-andrw'],
-				[ 'Porter Square'     ,     42.3884 , -71.11914899999999 , 'place-portr'],
-				[ 'Harvard Square'    ,   42.373362 ,         -71.118956 , 'place-harsq'],
-				[ 'JFK/UMass'         ,   42.320685 ,         -71.052391 , 'place-jfk'],
-				[ 'Savin Hill'        ,    42.31129 ,         -71.053331 , 'place-shmnl'],
-				 ['Park Street'       , 42.35639457 ,        -71.0624242 , 'place-pktrm'],
-				[ 'Broadway'          ,   42.342622 ,         -71.056967 , 'place-brdwy'],
-				[ 'North Quincy '     ,   42.275275 ,         -71.029583 , 'place-nqncy'],
-				[ 'Shawmut'           , 42.29312583 , -71.06573796000001 , 'place-smmnl'],
-				[ 'Davis'             ,    42.39674 ,         -71.121815 , 'place-davis'],
-				[ 'Alewife'           ,   42.395428 ,         -71.142483 , 'place-alfcl'],
-				[ 'Kendall/MIT '      , 42.36249079 ,       -71.08617653 , 'place-knncl'],
-				[ 'Charles/MGH'       ,   42.361166 ,         -71.070628 , 'place-chmnl'],
-				[ 'Downtown Crossing' ,   42.355518 ,         -71.060225 , 'place-dwnxg'],
-				[ 'Quincy Center'     ,   42.251809 ,         -71.005409 , 'place-qnctr'],
-				 [ 'Quincy Adams'      ,   42.233391 ,         -71.007153 , 'place-qamnl'],
-				 ['Ashmont'           ,   42.284652 , -71.06448899999999 , 'place-asmnl'],
-				 ['Wollaston'         ,  42.2665139 ,        -71.0203369 ,'place-wlsta'],
-				[ 'Fields Corner'     ,   42.300093 ,         -71.061667 , 'place-fldcr'],
-				[ 'Central Square'    ,   42.365486 ,         -71.103802 , 'place-cntsq'],
-				[ 'Braintree'        ,   42.2078543 ,        -71.0011385 , 'place-brntn']];
+					['Andrew'           ,   42.330154 ,         -71.057655 , 'place-andrw'],
+					[ 'Porter Square'     ,     42.3884 , -71.11914899999999 , 'place-portr'],
+					[ 'Harvard Square'    ,   42.373362 ,         -71.118956 , 'place-harsq'],
+					[ 'JFK/UMass'         ,   42.320685 ,         -71.052391 , 'place-jfk'],
+					[ 'Savin Hill'        ,    42.31129 ,         -71.053331 , 'place-shmnl'],
+				 	['Park Street'       , 42.35639457 ,        -71.0624242 , 'place-pktrm'],
+					[ 'Broadway'          ,   42.342622 ,         -71.056967 , 'place-brdwy'],
+					[ 'North Quincy '     ,   42.275275 ,         -71.029583 , 'place-nqncy'],
+					[ 'Shawmut'           , 42.29312583 , -71.06573796000001 , 'place-smmnl'],
+					[ 'Davis'             ,    42.39674 ,         -71.121815 , 'place-davis'],
+					[ 'Alewife'           ,   42.395428 ,         -71.142483 , 'place-alfcl'],
+					[ 'Kendall/MIT '      , 42.36249079 ,       -71.08617653 , 'place-knncl'],
+					[ 'Charles/MGH'       ,   42.361166 ,         -71.070628 , 'place-chmnl'],
+					[ 'Downtown Crossing' ,   42.355518 ,         -71.060225 , 'place-dwnxg'],
+					[ 'Quincy Center'     ,   42.251809 ,         -71.005409 , 'place-qnctr'],
+				 	[ 'Quincy Adams'      ,   42.233391 ,         -71.007153 , 'place-qamnl'],
+				 	['Ashmont'           ,   42.284652 , -71.06448899999999 , 'place-asmnl'],
+				 	['Wollaston'         ,  42.2665139 ,        -71.0203369 ,'place-wlsta'],
+					[ 'Fields Corner'     ,   42.300093 ,         -71.061667 , 'place-fldcr'],
+					[ 'Central Square'    ,   42.365486 ,         -71.103802 , 'place-cntsq'],
+					[ 'Braintree'        ,   42.2078543 ,        -71.0011385 , 'place-brntn']];
 
 var S_station = {lat: 42.352271, lng: -71.05524200000001};
 
@@ -27,8 +27,8 @@ var image = 'beachflag.png';
 var myLat = 0;
 var myLng = 0;
 var map;
-var me = new google.maps.LatLng(myLat, myLng);
-var infowindow = new google.maps.InfoWindow();
+var me;
+var infowindow;
 
 // Initialize and add the map
 function initMap() {;
@@ -47,7 +47,6 @@ function placeMarker(map){
         position: new google.maps.LatLng(locationList[i][1], locationList[i][2]),
         map: map,
         icon:image
-
       });
       //places a addlistener to each of the stations
       listen_stations(map, marker, locationList[i][0], locationList[i][3]);
@@ -153,7 +152,8 @@ function renderMap(myLat, myLng) {
 		  });
 		  pathTo.setMap(map);
 
-		var content = closest_to_me.stationName +"<br> " +  closest_to_me.station + " m";
+		var content = "closest station: " + closest_to_me.stationName +"<br> " +  
+		"distance from me: " + closest_to_me.station + " m";
 		infowindow.setContent(content);
 		infowindow.open(map, marker);
 	});
@@ -192,7 +192,10 @@ function listen_stations(map, marker, station, stop_id){
 function stationInfo(station_name, stop_id){
 	//Make instance of XHR object to make XML request
 		var request = new XMLHttpRequest();
-		var infowindow = new google.maps.InfoWindow();
+		var infowindow = new google.maps.InfoWindow({
+			maxWidth: 800,
+			height: 200
+		});
 		// Open the JSON file at remote location
 		request.open("GET", "https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id=place-davis", true);
 		infowindow.setContent("<p> Loading... </p>");
@@ -230,9 +233,8 @@ function stationInfo(station_name, stop_id){
 					else {
 						bounded_for = "Northbound";
 											}
-					content += "<div id = 'content'>"+"<h3>"+ "Arrival time: " + "</h3>" +
-					"<h4>" + arr[1]	+ "</h4>" + "<br>" +
-					"<h3>"+ "Departure time: " + "</h3>" + 
+					content += "<div id = 'content'>" +"<h3>"+ "Arrival time: " + "</h3>" + "<h4>" + arr[1]	+
+					 "</h4>" + "<h3>"+ "Departure time: " + "</h3>" + 
 					"<h4>"+ dep[1]	+ "</h4>" + "<br>" +
 					"<h3>"+ "Bounded for: "+ "</h3>" +
 					 "<h4>"+ bounded_for+ "</h4>" + "<br>"+ "</div>" ;	
